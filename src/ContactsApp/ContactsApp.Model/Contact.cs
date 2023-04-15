1897c654608d4e588f205c4f08d7d5617da16844
Tsukanov Semen <apple.semen@mail.ru>
2023-04-15 13:22:06 +0700

    feature(ContactsAppService): сервисный класс для методов преобразования строк.

--- src/ContactsApp/ContactsApp.Model/Contact.cs
+++ src/ContactsApp/ContactsApp.Model/Contact.cs
@@ -61,7 +61,7 @@ namespace ContactsApp.Model
                         $"{_maxNameAndEmailLength} characters.");
                 }
 
-                _fullName = FirstCharactersToUpperCase(value);
+                _fullName = ContactsAppService.FirstCharactersToUpperCase(value);
             }
         }
 
@@ -98,7 +98,7 @@ namespace ContactsApp.Model
             }
             set
             {
-                _phoneNumber = FilterStringPhoneNumber(value);
+                _phoneNumber = ContactsAppService.FilterStringPhoneNumber(value);
             }
         }
 
@@ -164,10 +164,10 @@ namespace ContactsApp.Model
         /// <param name="dayOfBirth">Дата рождения.</param>
         /// <param name="idVK">ID контакта.</param>
         public Contact(
-            string fullName, 
+            string fullName,
             string email,
-            string phoneNumber, 
-            DateTime dayOfBirth, 
+            string phoneNumber,
+            DateTime dayOfBirth,
             string idVK)
         {
             FullName = fullName;
@@ -175,36 +175,6 @@ namespace ContactsApp.Model
             PhoneNumber = phoneNumber;
             DayOfBirth = dayOfBirth;
             IdVK = idVK;
-        }
-
-        /// <summary>
-        /// Преобразует первые символы слов в заглавные.
-        /// </summary>
-        /// <param name="value">Строка слов.</param>
-        /// <returns>Обновленная строка с заглавными символами.</returns>
-        private string FirstCharactersToUpperCase(string value)
-        {
-            string[] names = value.Split(' ');
-
-            for (int i = 0; i < names.Length; i++)
-            {
-                var name = names[i];
-                names[i] = name.Substring(0, 1).ToUpper() + name.Substring(1);
-            }
-
-            return String.Join(" ", names);
-        }
-
-        /// <summary>
-        /// Фильтрует строку символов телефонного номера.
-        /// </summary>
-        /// <param name="value">Входная строка символов.</param>
-        /// <returns>Выходная строка.</returns>
-        private string FilterStringPhoneNumber(string value)
-        {
-            string allowedChars = "1234567890+()- ";
-            return new string(value.Where(character =>
-            allowedChars.Contains(character)).ToArray());
         }
 
         /// <summary>
---
+++ src/ContactsApp/ContactsApp.Model/ContactsAppService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Model
+{
+    public class ContactsAppService
+    {
+        /// <summary>
+        /// Преобразует первые символы слов в заглавные.
+        /// </summary>
+        /// <param name="value">Строка слов.</param>
+        /// <returns>Обновленная строка с заглавными символами.</returns>
+        public static string FirstCharactersToUpperCase(string value)
+        {
+            string[] names = value.Split(' ');
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                var name = names[i];
+                names[i] = name.Substring(0, 1).ToUpper() + name.Substring(1);
+            }
+
+            return String.Join(" ", names);
+        }
+
+        /// <summary>
+        /// Фильтрует строку символов телефонного номера.
+        /// </summary>
+        /// <param name="value">Входная строка символов.</param>
+        /// <returns>Выходная строка.</returns>
+        public static string FilterStringPhoneNumber(string value)
+        {
+            string allowedChars = "1234567890+()- ";
+            return new string(value.Where(character =>
+            allowedChars.Contains(character)).ToArray());
+        }
+
+    }
+}
