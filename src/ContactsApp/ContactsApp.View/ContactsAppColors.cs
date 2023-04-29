179b0046123586c3fb897ee4b75ecfae244165bc
Tsukanov Semen <apple.semen@mail.ru>
2023-04-29 12:36:10 +0700

    refactor(ContactsAppColors): класс вынесен во view.

--- src/ContactsApp/ContactsApp.View/ContactsAppColors.cs
+++ src/ContactsApp/ContactsApp.View/ContactsAppColors.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ContactsApp.Model
+namespace ContactsApp.View
 {
     /// <summary>
     /// Класс цветов приложения.
