f70f5efd98298763a0e747251fcfa550b6b29852
Tsukanov Semen <apple.semen@mail.ru>
2023-05-02 18:07:43 +0700

    feature(ContactTest): Добавлен тесты на сеттер полного имени, не раобтают на данный момент.

--- src/ContactsApp/ContactsApp.Model.UnitTests/ContactTest.cs
+++ src/ContactsApp/ContactsApp.Model.UnitTests/ContactTest.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ContactsApp.Model.UnitTests
 {
+    [TestFixture]
     public class ContactTest
     {
+        [TestCase("", "An exception should be thrown if the length " +
+            "of the full name is 0.", TestName = "Assigning an empty string to full name.")]
+        [TestCase("The very very very very very very very very very very very very very " +
+            "very very very very very long name which greater then 100 characters.", TestName =
+            "Assigning a string greater than 100 characters.")]
+        public void Test_FullNameSet_ArgumentException(string wrongFullName, string message)
+        {
+            var contact = new Contact();
+            Assert.Throws<ArgumentException>(() =>
+            { contact.FullName = wrongFullName; }, message);
+        }
+
     }
 }
