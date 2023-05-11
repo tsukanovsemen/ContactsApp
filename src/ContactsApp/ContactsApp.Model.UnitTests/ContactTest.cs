ef74eff6d600d9fb39960909341cc942b43962c2
Tsukanov Semen <apple.semen@mail.ru>
2023-05-12 01:16:47 +0700

    feature(ProjectTest): добавлены тесты на методы сортировки и поиска именинника.

--- src/ContactsApp/ContactsApp.Model.UnitTests/ContactTest.cs
+++ src/ContactsApp/ContactsApp.Model.UnitTests/ContactTest.cs
@@ -26,20 +26,21 @@ namespace ContactsApp.Model.UnitTests
             "An exception should be thrown if the length " +
             "of the full name is greater than 100 characters.", TestName =
             "Assigning a string greater than 100 characters.")]
-        public void Test_FullNameSet_ArgumentException(string wrongFullName, string message)
+        public void FullName_SetIncorrectValue_ArgumentException(string wrongFullName, string message)
         {
             Assert.Throws<ArgumentException>(() =>
             { _contact.FullName = wrongFullName; }, message);
         }
 
         [Test(Description = "Positive full name assignment check.")]
-        public void Test_FullNameSet_CorrectSet()
+        public void FullName_SetCorrectValue_ReturnsSameValue()
         {
             //Arrange
-            var expected = "Tsukanov Semyon Sergeevich";
+            var correctFullName = "Tsukanov Semyon Sergeevich";
+            var expected = correctFullName;
 
             //Act
-            _contact.FullName = "Tsukanov Semyon Sergeevich";
+            _contact.FullName = correctFullName;
             var actual = _contact.FullName;
 
             //Assert
@@ -47,7 +48,7 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative email assignment check.")]
-        public void Test_EmailSet_ArgumentException()
+        public void Email_SetIncorrectValue_ArgumentException()
         {
             var wrongEmail = "The very very very very very very very" +
                 "very very very very very very very very very very very very very big email.";
@@ -59,13 +60,14 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive email assignment check.")]
-        public void Test_EmailSet_CorrectSet()
+        public void Email_SetCorrectValue_ReturnsSameValue()
         {
             //Arrange
-            var expected = "sementsukanovvv@gmail.com";
+            var correctEmail = "sementsukanovvv@gmail.com";
+            var expected = correctEmail;
 
             //Act
-            _contact.Email = "sementsukanovvv@gmail.com";
+            _contact.Email = correctEmail;
             var actual = _contact.Email;
 
             //Assert
@@ -73,13 +75,15 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive validation of email assignment to small characters.")]
-        public void Test_EmailSet_AllCharsAreLower()
+        public void Email_SetAllCharsAreLower_ReturnsAllCharsLowerValue()
         {
             //Arrange
-            var expected = "sementsukanovvv@gmail.com";
+            var lowerCorrectEmail = "sementsukanovvv@gmail.com";
+            var expected = lowerCorrectEmail;
 
             //Act
-            _contact.Email = "SEMENTSUKANOVVV@GMAIL.COM";
+            var upperIncorrectEmail = lowerCorrectEmail.ToUpper();
+            _contact.Email = upperIncorrectEmail;
             var actual = _contact.Email;
 
             //Assert
@@ -87,13 +91,14 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative phone number assignment check.")]
-        public void Test_PhoneNumberSet_EmptyString()
+        public void PhoneNumber_SetEmptyValue_EmptyString()
         {
             //Arrange
-            var expected = " ";
+            var emptyString = " ";
+            var expected = emptyString;
 
             //Act
-            _contact.PhoneNumber = "";
+            _contact.PhoneNumber = string.Empty;
             var actual = _contact.PhoneNumber;
 
             //Assert
@@ -101,7 +106,7 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative phone number assignment check.")]
-        public void Test_PhoneNumberSet_ArgumentException()
+        public void PhoneNumber_SetIncorrectValue_ArgumentException()
         {
             var wrongPhoneNumber = "abcdef";
             var message = "An exception should be thrown if the chars of " +
@@ -112,13 +117,14 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive phone number assignment check.")]
-        public void Test_PhoneNumberSet_CorrectSet()
+        public void PhoneNumber_SetCorrectValue_ReturnsSameValue()
         {
             //Arrange
-            var expected = "+7(909)145-13-12";
+            var correctPhoneNumber = "+7(909)145-13-12";
+            var expected = correctPhoneNumber;
 
             //Act
-            _contact.PhoneNumber = "+7(909)145-13-12";
+            _contact.PhoneNumber = correctPhoneNumber;
             var actual = _contact.PhoneNumber;
 
             //Assert
@@ -126,7 +132,7 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative date of birth assignment check.")]
-        public void Test_DateOfBirthSetLittleDate_ArgumetException()
+        public void DateOfBirth_SetLittleDate_ArgumetException()
         {
             var wrongDate = new DateTime(1899, 1, 1);
             var message = "An exception should be thrown if the date of " +
@@ -137,7 +143,7 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative date of birth assignment check.")]
-        public void Test_DateOfBirthSetTomorrowDate_ArgumetException()
+        public void DateOfBirth_SetTomorrowDate_ArgumetException()
         {
             var wrongDate = DateTime.Now.AddDays(1);
             var message = "An exception should be thrown if the date of " +
@@ -148,13 +154,14 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive date of birth assignment check.")]
-        public void Test_DateOfBirthSet_CorrectSet()
+        public void DateOfBirth_SetCorrectValue_ReturnsSameValue()
         {
             //Arrange
-            var expected = new DateTime(2022, 3, 3);
+            var correctDate = new DateTime(2022, 3, 3);
+            var expected = correctDate;
 
             //Act
-            _contact.DateOfBirth = new DateTime(2022, 3, 3);
+            _contact.DateOfBirth = correctDate;
             var actual = _contact.DateOfBirth;
 
             //Assert
@@ -162,7 +169,7 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative IdVk assignment check.")]
-        public void Test_IdVkSet_ArgumentException()
+        public void IdVk_SetBigLengthValue_ArgumentException()
         {
             var wrongIdVk = " big id:0123456789101112131415161718192021222324252627282930";
             var message = "An exception should be thrown if the id vk " +
@@ -173,13 +180,14 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive IdVk assignment check.")]
-        public void Test_IdVkSet_EmptyString()
+        public void IdVk_SetEmptyValue_EmptyString()
         {
             //Avarrage
-            var expected = " ";
+            var emptyString = " ";
+            var expected = emptyString;
 
             //Act
-            _contact.IdVK = "";
+            _contact.IdVK = string.Empty;
             var actual = _contact.IdVK;
 
             //Assert
@@ -187,13 +195,14 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive IdVk assignment check.")]
-        public void Test_IdVkSet_CorrectSet()
+        public void IdVk_SetCorrectValue_ReturnsSameValue()
         {
             //Avarrage
-            var expected = "id@/semyon";
+            var correctId = "id@/semyon";
+            var expected = correctId;
 
             //Act
-            _contact.IdVK = "id@/semyon";
+            _contact.IdVK = correctId;
             var actual = _contact.IdVK;
 
             //Assert
@@ -201,7 +210,7 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Negative Constructor assignment check.")]
-        public void Test_ContactConstructor_ArgumentException()
+        public void Constructor_PassingInvalidParameter_ArgumentException()
         {
             var wrongFullName = "very very very very very very very very" +
                 "very very very very very very very very very very very very very big letter.";
@@ -209,26 +218,37 @@ namespace ContactsApp.Model.UnitTests
                 "the argument of constructor is incorrect.";
 
             Assert.Throws<ArgumentException>(() =>
-            { new Contact(wrongFullName, "email","55-55-55",
-                new DateTime(2022,1,1),""); }, message);
+            {
+                new Contact(wrongFullName, "email", "55-55-55",
+                new DateTime(2022, 1, 1), "");
+            }, message);
         }
 
         [Test(Description = "Positive Constructor assignment check.")]
-        public void Test_ContactConstructor_Correct()
+        public void Constructor_PassingCorrectParameters_ReturnsSameValues()
         {
             //Avarrage
-            var expectedFullName = "Semyon Tsukanov";
-            var expectedEmail = "email@mail.com";
-            var expectedPhoneNumber = "89095053939";
-            var expectedDateOfBirth = new DateTime(2001, 11, 6);
-            var expectedIdVk = "id@/vk";
+            var fullName = "Semyon Tsukanov";
+            var expectedFullName = fullName;
+
+            var email = "email@mail.com";
+            var expectedEmail = email;
+
+            var phoneNumber = "89095053939";
+            var expectedPhoneNumber = phoneNumber;
+
+            var date = new DateTime(2001, 11, 6);
+            var expectedDateOfBirth = date;
+
+            var idVk = "id@/vk";
+            var expectedIdVk = idVk;
 
             //Act
-            Contact contact = new Contact("Semyon Tsukanov",
-                "email@mail.com", 
-                "89095053939",
-                new DateTime(2001, 11, 6),
-                "id@/vk");
+            Contact contact = new Contact(fullName,
+                email,
+                phoneNumber,
+                date,
+                idVk);
             var actualFullName = contact.FullName;
             var actualEmail = contact.Email;
             var actualPhoneNumber = contact.PhoneNumber;
@@ -244,21 +264,30 @@ namespace ContactsApp.Model.UnitTests
         }
 
         [Test(Description = "Positive Clone method check.")]
-        public void Test_CloneMethod_ClonedInstance()
+        public void CloneMethod_CloneObject_ReturnsSameValues()
         {
             //Avarrange
-            var expectedFullName = "Semyon Tsukanov";
-            var expectedEmail = "email@mail.com";
-            var expectedPhoneNumber = "89095053939";
-            var expectedDateOfBirth = new DateTime(2001, 11, 6);
-            var expectedIdVk = "id@/vk";
+            var fullName = "Semyon Tsukanov";
+            var expectedFullName = fullName;
+
+            var email = "email@mail.com";
+            var expectedEmail = email;
+
+            var phoneNumber = "89095053939";
+            var expectedPhoneNumber = phoneNumber;
+
+            var date = new DateTime(2001, 11, 6);
+            var expectedDateOfBirth = date;
+
+            var idVk = "id@/vk";
+            var expectedIdVk = idVk;
 
             //Act
-            _contact = new Contact("Semyon Tsukanov",
-                "email@mail.com",
-                "89095053939",
-                new DateTime(2001, 11, 6),
-                "id@/vk");
+            _contact = new Contact(fullName,
+                email,
+                phoneNumber,
+                date,
+                idVk);
 
             Contact newContact = (Contact)_contact.Clone();
             var actualFullName = newContact.FullName;
---
+++ src/ContactsApp/ContactsApp.Model.UnitTests/ProjectTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ContactsApp.Model.UnitTests
+{
+    [TestFixture]
+    public class ProjectTest
+    {
+        Project _project;
+
+        [SetUp]
+        public void InitializeProject()
+        {
+            _project = new Project();
+        }
+
+        [Test(Description = "Positive get contacts test.")]
+        public void Contacts_GetContacts_ReturnsSameValue()
+        {
+            //Avarrage
+            var fullNameFirst = "Sem";
+            var fullNameSecond = "Artem";
+            var expectedFullNameFirstContact = fullNameFirst;
+            var expectedFullNameSecondContact = fullNameSecond;
+
+            //Act
+            Contact firstContact = new Contact();
+            firstContact.FullName = fullNameFirst;
+            _project.Contacts.Add(firstContact);
+
+            Contact secondContact = new Contact();
+            secondContact.FullName = fullNameSecond;
+            _project.Contacts.Add(secondContact);
+
+            var actualFullNameFirstContact = _project.Contacts[0].FullName;
+            var actualFullNameSecondContact = _project.Contacts[1].FullName;
+
+            //Assert
+            Assert.AreEqual(expectedFullNameFirstContact, actualFullNameFirstContact);
+            Assert.AreEqual(expectedFullNameSecondContact, actualFullNameSecondContact);
+        }
+
+        [Test(Description = "Positive test of sorting contacts")]
+        public void SortByNameMethod_SortContacts_SortedList()
+        {
+            //Avarrage
+            List<Contact> alreadySortedContacts = new List<Contact>();
+
+            Contact firstContact = new Contact();
+            var firstContactName = "Artem";
+            firstContact.FullName = firstContactName;
+
+            Contact secondContact = new Contact();
+            var secondContactName = "Boris";
+            secondContact.FullName = secondContactName;
+
+            Contact thirdContact = new Contact();
+            var thirdContactName = "Vladimir";
+            thirdContact.FullName = thirdContactName;
+
+            alreadySortedContacts.Add(firstContact);
+            alreadySortedContacts.Add(secondContact);
+            alreadySortedContacts.Add(thirdContact);
+
+            var expectedContactsList = alreadySortedContacts;
+
+            //Act
+            List<Contact> contacts = new List<Contact>();
+            contacts.Add(secondContact);
+            contacts.Add(thirdContact);
+            contacts.Add(firstContact);
+            var actualContactsList = _project.SortContactsByName(contacts);
+
+            //Assert
+            Assert.AreEqual(expectedContactsList, actualContactsList);
+        }
+
+        [Test(Description = "Positive test of finding birthday contacts.")]
+        public void FindBirthdayContactsMethod_FindContacts_ReturnsBirthdayList()
+        {
+            //Avarrage
+            var birthdayContactsList = new List<Contact>();
+            var birthdayContact = new Contact();
+            var birthDate = DateTime.Today;
+            birthdayContact.DateOfBirth = birthDate;
+            birthdayContactsList.Add(birthdayContact);
+            var expectedContactsList = birthdayContactsList;
+
+            //Act
+            var contactsList = new List<Contact>();
+            var firstContact = new Contact();
+            var someDate = birthDate.AddDays(-3);
+            firstContact.DateOfBirth = someDate;
+            contactsList.Add(firstContact);
+
+            var secondContact = new Contact();
+            secondContact.DateOfBirth = birthDate;
+            contactsList.Add(secondContact);
+            var actualContactsList = _project.FindBirthdayContacts(contactsList);
+
+            //Assert
+            Assert.AreEqual(expectedContactsList[0].DateOfBirth, 
+                actualContactsList[0].DateOfBirth);
+        }
+    }
+}
