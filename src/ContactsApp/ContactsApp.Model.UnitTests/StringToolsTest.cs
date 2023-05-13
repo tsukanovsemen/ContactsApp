1dba8c3fc7fcec030d6be06b2ca797601de25d8f
Tsukanov Semen <apple.semen@mail.ru>
2023-05-13 10:29:47 +0700

    feature(StringToolsTest): покрыт тестами класс StringTools.

---
+++ src/ContactsApp/ContactsApp.Model.UnitTests/StringToolsTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ContactsApp.Model.UnitTests
+{
+    [TestFixture]
+    public class StringToolsTest
+    {
+        [Test(Description = "Positive test of converting lower letters to upper ones.")]
+        public void FirtsCharactersToUpperCaseMethod_ConvertString_ConvertedString()
+        {
+            //Avarrage
+            var convertedText = "Some Text";
+            var expectedText = convertedText;
+
+            //Act
+            var convertingText = "some text";
+            var actualText = StringTools.FirstCharactersToUpperCase(convertingText);
+
+            //Assert
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [Test(Description = "Positive test of validation phone number.")]
+        public void ValidationPhoneNumberCorrect_Validation_ReturnsTrue()
+        {
+            //Avarrage
+            var expectedResult = true;
+
+            //Act
+            var value = "1234567890()-";
+            var actualResult = StringTools.IsPhoneNumberCorrect(value);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test(Description = "Negative test of validation phone number.")]
+        public void ValidationPhoneNumberIncorrect_Validation_ReturnsFalse()
+        {
+            //Avarrage
+            var expectedResult = false;
+
+            //Act
+            var value = "Hello World.";
+            var actualResult = StringTools.IsPhoneNumberCorrect(value);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
