3d9dc337730ea72f19e27d538e1f593957d183ea
Tsukanov Semen <apple.semen@mail.ru>
2023-05-13 12:25:39 +0700

    refactor(ContactTests): поравлены небольши ошибки, убран Setup.

--- src/ContactsApp/ContactsApp.Model.UnitTests/ContactTests.cs
+++ src/ContactsApp/ContactsApp.Model.UnitTests/ContactTests.cs
@@ -8,16 +8,8 @@ using NUnit.Framework;
 namespace ContactsApp.Model.UnitTests
 {
     [TestFixture]
-    public class ContactTest
+    public class ContactTests
     {
-        private Contact _contact;
-
-        [SetUp]
-        public void InitializeContact()
-        {
-            _contact = new Contact();
-        }
-
         [Test(Description = "Negative full name assignment check.")]
         [TestCase("", "An exception should be thrown if the length " +
             "of the full name is 0.", TestName = "Assigning an empty string to full name.")]
@@ -28,182 +20,200 @@ namespace ContactsApp.Model.UnitTests
             "Assigning a string greater than 100 characters.")]
         public void FullName_SetIncorrectValue_ArgumentException(string wrongFullName, string message)
         {
+            // Setup
+            Contact contact = new Contact();
+
+            // Assert
             Assert.Throws<ArgumentException>(() =>
-            { _contact.FullName = wrongFullName; }, message);
+            {
+                // Act
+                contact.FullName = wrongFullName;
+            },
+            message);
         }
 
         [Test(Description = "Positive full name assignment check.")]
         public void FullName_SetCorrectValue_ReturnsSameValue()
         {
-            //Arrange
+            // Setup
+            Contact contact = new Contact();
             var correctFullName = "Tsukanov Semyon Sergeevich";
             var expected = correctFullName;
 
-            //Act
-            _contact.FullName = correctFullName;
-            var actual = _contact.FullName;
+            // Act
+            contact.FullName = correctFullName;
+            var actual = contact.FullName;
 
-            //Assert
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test(Description = "Negative email assignment check.")]
         public void Email_SetIncorrectValue_ArgumentException()
         {
+            // Setup
+            Contact contact = new Contact();
             var wrongEmail = "The very very very very very very very" +
                 "very very very very very very very very very very very very very big email.";
             var message = "An exception should be thrown if the length " +
             "of the email is greater than 100 characters.";
 
+            // Assert
             Assert.Throws<ArgumentException>(() =>
-            { _contact.FullName = wrongEmail; }, message);
+            {
+                // Act
+                contact.FullName = wrongEmail;
+            },
+            message);
         }
 
         [Test(Description = "Positive email assignment check.")]
         public void Email_SetCorrectValue_ReturnsSameValue()
         {
-            //Arrange
+            // Setup
+            Contact contact = new Contact();
             var correctEmail = "sementsukanovvv@gmail.com";
             var expected = correctEmail;
 
-            //Act
-            _contact.Email = correctEmail;
-            var actual = _contact.Email;
+            // Act
+            contact.Email = correctEmail;
+            var actual = contact.Email;
 
-            //Assert
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test(Description = "Positive validation of email assignment to small characters.")]
         public void Email_SetAllCharsAreLower_ReturnsAllCharsLowerValue()
         {
-            //Arrange
+            // Setup
+            Contact contact = new Contact();
             var lowerCorrectEmail = "sementsukanovvv@gmail.com";
             var expected = lowerCorrectEmail;
 
-            //Act
+            // Act
             var upperIncorrectEmail = lowerCorrectEmail.ToUpper();
-            _contact.Email = upperIncorrectEmail;
-            var actual = _contact.Email;
+            contact.Email = upperIncorrectEmail;
+            var actual = contact.Email;
 
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test(Description = "Negative phone number assignment check.")]
-        public void PhoneNumber_SetEmptyValue_EmptyString()
-        {
-            //Arrange
-            var emptyString = " ";
-            var expected = emptyString;
-
-            //Act
-            _contact.PhoneNumber = string.Empty;
-            var actual = _contact.PhoneNumber;
-
-            //Assert
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test(Description = "Negative phone number assignment check.")]
         public void PhoneNumber_SetIncorrectValue_ArgumentException()
         {
+            // Setup
+            Contact contact = new Contact();
             var wrongPhoneNumber = "abcdef";
             var message = "An exception should be thrown if the chars of " +
                 "the phone number are incorrect.";
 
             Assert.Throws<ArgumentException>(() =>
-            { _contact.PhoneNumber = wrongPhoneNumber; }, message);
+            {
+                // Act
+                contact.PhoneNumber = wrongPhoneNumber;
+            },
+            message);
         }
 
         [Test(Description = "Positive phone number assignment check.")]
         public void PhoneNumber_SetCorrectValue_ReturnsSameValue()
         {
-            //Arrange
+            // Setup
+            Contact contact = new Contact();
             var correctPhoneNumber = "+7(909)145-13-12";
             var expected = correctPhoneNumber;
 
-            //Act
-            _contact.PhoneNumber = correctPhoneNumber;
-            var actual = _contact.PhoneNumber;
+            // Act
+            contact.PhoneNumber = correctPhoneNumber;
+            var actual = contact.PhoneNumber;
 
-            //Assert
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test(Description = "Negative date of birth assignment check.")]
         public void DateOfBirth_SetLittleDate_ArgumetException()
         {
+            // Setup
+            Contact contact = new Contact();
             var wrongDate = new DateTime(1899, 1, 1);
             var message = "An exception should be thrown if the date of " +
                 "birth is less than 1900.1.1.";
 
+            // Assert
             Assert.Throws<ArgumentException>(() =>
-            { _contact.DateOfBirth = wrongDate; }, message);
+            {
+                // Act
+                contact.DateOfBirth = wrongDate;
+            },
+            message);
         }
 
         [Test(Description = "Negative date of birth assignment check.")]
         public void DateOfBirth_SetTomorrowDate_ArgumetException()
         {
+            // Setup
+            Contact contact = new Contact();
             var wrongDate = DateTime.Now.AddDays(1);
             var message = "An exception should be thrown if the date of " +
                 "birth is greater than date now.";
 
+            // Assert
             Assert.Throws<ArgumentException>(() =>
-            { _contact.DateOfBirth = wrongDate; }, message);
+            {
+                // Act
+                contact.DateOfBirth = wrongDate;
+            },
+            message);
         }
 
         [Test(Description = "Positive date of birth assignment check.")]
         public void DateOfBirth_SetCorrectValue_ReturnsSameValue()
         {
-            //Arrange
+            // Setup
+            Contact contact = new Contact();
             var correctDate = new DateTime(2022, 3, 3);
             var expected = correctDate;
 
-            //Act
-            _contact.DateOfBirth = correctDate;
-            var actual = _contact.DateOfBirth;
+            // Act
+            contact.DateOfBirth = correctDate;
+            var actual = contact.DateOfBirth;
 
-            //Assert
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test(Description = "Negative IdVk assignment check.")]
         public void IdVk_SetBigLengthValue_ArgumentException()
         {
+            // Setup
+            Contact contact = new Contact();
             var wrongIdVk = " big id:0123456789101112131415161718192021222324252627282930";
             var message = "An exception should be thrown if the id vk " +
                 "will be greater than 50 characters.";
 
+            // Assert
             Assert.Throws<ArgumentException>(() =>
-            { _contact.IdVK = wrongIdVk; }, message);
-        }
-
-        [Test(Description = "Positive IdVk assignment check.")]
-        public void IdVk_SetEmptyValue_EmptyString()
-        {
-            //Avarrage
-            var emptyString = " ";
-            var expected = emptyString;
-
-            //Act
-            _contact.IdVK = string.Empty;
-            var actual = _contact.IdVK;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
+            {
+                // Act
+                contact.IdVK = wrongIdVk;
+            },
+            message);
         }
 
         [Test(Description = "Positive IdVk assignment check.")]
         public void IdVk_SetCorrectValue_ReturnsSameValue()
         {
-            //Avarrage
+            // Setup
+            Contact contact = new Contact();
             var correctId = "id@/semyon";
             var expected = correctId;
 
             //Act
-            _contact.IdVK = correctId;
-            var actual = _contact.IdVK;
+            contact.IdVK = correctId;
+            var actual = contact.IdVK;
 
             //Assert
             Assert.AreEqual(expected, actual);
@@ -212,22 +222,26 @@ namespace ContactsApp.Model.UnitTests
         [Test(Description = "Negative Constructor assignment check.")]
         public void Constructor_PassingInvalidParameter_ArgumentException()
         {
+            // Setup
             var wrongFullName = "very very very very very very very very" +
                 "very very very very very very very very very very very very very big letter.";
             var message = "An exception should be thrown if " +
                 "the argument of constructor is incorrect.";
 
+            // Assert
             Assert.Throws<ArgumentException>(() =>
             {
+                // Act
                 new Contact(wrongFullName, "email", "55-55-55",
                 new DateTime(2022, 1, 1), "");
-            }, message);
+            },
+            message);
         }
 
         [Test(Description = "Positive Constructor assignment check.")]
         public void Constructor_PassingCorrectParameters_ReturnsSameValues()
         {
-            //Avarrage
+            // Setup
             var fullName = "Semyon Tsukanov";
             var expectedFullName = fullName;
 
@@ -243,19 +257,20 @@ namespace ContactsApp.Model.UnitTests
             var idVk = "id@/vk";
             var expectedIdVk = idVk;
 
-            //Act
             Contact contact = new Contact(fullName,
                 email,
                 phoneNumber,
                 date,
                 idVk);
+
+            // Act
             var actualFullName = contact.FullName;
             var actualEmail = contact.Email;
             var actualPhoneNumber = contact.PhoneNumber;
             var actualDateOfBirth = contact.DateOfBirth;
             var actualIdVk = contact.IdVK;
 
-            //Assert
+            // Assert
             Assert.AreEqual(expectedFullName, actualFullName);
             Assert.AreEqual(expectedEmail, actualEmail);
             Assert.AreEqual(expectedPhoneNumber, actualPhoneNumber);
@@ -266,7 +281,7 @@ namespace ContactsApp.Model.UnitTests
         [Test(Description = "Positive Clone method check.")]
         public void CloneMethod_CloneObject_ReturnsSameValues()
         {
-            //Avarrange
+            // Setup
             var fullName = "Semyon Tsukanov";
             var expectedFullName = fullName;
 
@@ -282,21 +297,21 @@ namespace ContactsApp.Model.UnitTests
             var idVk = "id@/vk";
             var expectedIdVk = idVk;
 
-            //Act
-            _contact = new Contact(fullName,
+            Contact contact = new Contact(fullName,
                 email,
                 phoneNumber,
                 date,
                 idVk);
 
-            Contact newContact = (Contact)_contact.Clone();
+            // Act
+            Contact newContact = (Contact)contact.Clone();
             var actualFullName = newContact.FullName;
             var actualEmail = newContact.Email;
             var actualPhoneNumber = newContact.PhoneNumber;
             var actualDateOfBirth = newContact.DateOfBirth;
             var actualIdVk = newContact.IdVK;
 
-            //Assert
+            // Assert
             Assert.AreEqual(expectedFullName, actualFullName);
             Assert.AreEqual(expectedEmail, actualEmail);
             Assert.AreEqual(expectedPhoneNumber, actualPhoneNumber);
